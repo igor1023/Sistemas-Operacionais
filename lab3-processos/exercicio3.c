221c750f02bfcedb03b3c0327d0813bc96c60f55
Aluno <igor.s10@aluno.ifsc.edu.br>
2026-03-17 13:55:44 -0300

    feita funcao para validar entrada do teclado

--- lab3-processos/exercicio3.c
+++ lab3-processos/exercicio3.c
@@ -4,7 +4,8 @@ Implemente um terminal que leia caminhos completos de programas e execute-os usa
 execve(). Utilize wait() ou não dependendo se o comando termina com &. O laço deve continuar
 até que o usuário digite sair.
 */
-// se NAO tiver & => usa wait
+// Bloqueia entrada se NÃO tiver & => usa wait
+// Libera entrada se tiver &
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,23 +14,64 @@ até que o usuário digite sair.
 #include <sys/wait.h>
 #include<stdbool.h>
 #include<string.h>
+#include<ctype.h>
+
+#define MAX 256
+
+bool entrada_valida(char entrada[], bool* ecomercial);
+
+// vou pegar a entrada, o que for caminho joga para A, se tiver & joga em B
+bool entrada_valida(char entrada[], bool* ecomercial){
+
+    char *caminho = &entrada[0];
+    *ecomercial = false; //assumo que nao tem &
+
+    int i = strlen(entrada);
+    while(i >= 0){
+
+        if(caminho[i] == '&'){
+
+            *ecomercial = true;
+            
+            i--;
+            while(caminho[i] == ' ') // remover espaços
+                i--;
+
+            caminho[++i] = '\0';
+            break;
+
+        }
+        i--;
+    }
+
+    // false: sem caminho e/ou sem &
+    // true: com caminho valido e com &
+    return caminho != NULL && (caminho[0] != '\0' || caminho[0] != "") && *ecomercial;
+}
 
 int main(int argc, char** argv){
 
-    char caminho[256];
-    char and[5];
+    char entrada[MAX];
+    bool ecomercial = false;
 
     while(true){
 
         printf("> ");
-        scanf("%s %s", caminho, and);
+        fgets(entrada, MAX, stdin);
+        entrada[strlen(entrada)-1] = '\0'; // remove a quebra de linha
 
         // pegar a entrada com uma string apenas e separar com ponteiros
 
-        if(strcmp(caminho, "sair") == 0)
+        if(!entrada_valida(entrada, &ecomercial))
+            exit(-1);
+
+        if(strcmp(entrada, "sair") == 0)
             break;
 
-        printf("entrou com %s %s\n", caminho, and);
+        // neste ponto, entrada é diferente de "sair"
+        // e ja sabemos se tem ou nao tem '&'
+
+        
 
     }
 
